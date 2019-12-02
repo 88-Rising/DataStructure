2cae99f9e77ace5a57e24648c34c4f93c2206f86
88-Rising <348514635@qq.com>
2019-12-03 01:30:40 +0800

    自定义BinaryTree的各种遍历方法及其常用方法

--- MyTree/LinkedBinaryTree.java
+++ MyTree/LinkedBinaryTree.java
@@ -21,4 +21,32 @@ public class LinkedBinaryTree implements BinaryTree {
     public int getHeight() {
         return 0;
     }
+
+
+    public void preOderTraverse(){
+        System.out.println("先序遍历");
+        this.preOderTraverse(root);
+        System.out.println();
+
+    }
+    @Override
+    public void preOderTraverse(Node root) {
+        /*
+        * 1.输出根节点的值
+        * 2.对左子树进行先序遍历
+        * 3.对右子树进行先序遍历
+        * */
+        if(root!=null) {
+            System.out.print(root.value+"  ");
+
+
+            this.preOderTraverse(root.leftChild);
+
+          this.preOderTraverse(root.rightChild);
+
+        }
+
+
+
+    }
 }
