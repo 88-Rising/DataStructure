fde5afb9b2b341238230640354be3728ed5061b5
88-Rising <348514635@qq.com>
2019-12-03 01:10:06 +0800

    LinkedBinaryTree的接口

---
+++ MyTree/BinaryTree.java
@@ -0,0 +1,14 @@
+package MyTree;
+
+public interface BinaryTree {
+    //判断是否为空树
+    public Boolean isEmpty();
+
+    //树结点数量
+    public int size();
+
+    //获取树的高度
+    public int getHeight();
+
+
+}
