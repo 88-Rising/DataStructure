de785212a5c2710fb1051a1a1c9b3ecc19bc9e4e
88-Rising <348514635@qq.com>
2019-12-03 00:43:07 +0800

    定义树的结点

--- MyTree/Node.java
+++ MyTree/Node.java
@@ -5,10 +5,24 @@ public class Node {
     Node leftChild;
     Node rightChild;
 
+    public Node(Object value){
+        super();
+        this.value=value;
+    }
+
     public Node(Object value, Node leftChild, Node rightChild) {
         this.value = value;
         this.leftChild = leftChild;
         this.rightChild = rightChild;
 
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", leftChild=" + leftChild +
+                ", rightChild=" + rightChild +
+                '}';
     }
 }
