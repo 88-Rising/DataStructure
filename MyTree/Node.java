33e964040b192f6709a44495ee4a366707af5241
88-Rising <348514635@qq.com>
2019-12-03 00:38:25 +0800

    定义树的结点

---
+++ MyTree/Node.java
@@ -0,0 +1,14 @@
+package MyTree;
+
+public class Node {
+    Object value;
+    Node leftChild;
+    Node rightChild;
+
+    public Node(Object value, Node leftChild, Node rightChild) {
+        this.value = value;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+
+    }
+}
