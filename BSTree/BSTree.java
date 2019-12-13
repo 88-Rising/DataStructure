b1145e1771e0ca37898033891814c6d787e005d6
88-Rising <348514635@qq.com>
2019-12-13 23:08:44 +0800

    定义BSTree(二叉搜索树)的结点

---
+++ BSTree/BSTree.java
@@ -0,0 +1,33 @@
+package BSTree;
+
+public class BSTree <T extends Comparable<T>>{
+
+
+    /*
+    * 二叉搜索树需满足以下四个条件：
+
+    若任意节点的左子树不空，则左子树上所有结点的值均小于它的根结点的值；
+    若任意节点的右子树不空，则右子树上所有结点的值均大于它的根结点的值；
+    任意节点的左、右子树也分别为二叉查找树；
+    没有键值相等的节点。
+    *
+    * */
+
+        private BSTNode<T> mRoot;//定义根节点
+        public class BSTNode<T extends Comparable<T>>{
+            T key;   //定义键值
+            BSTNode<T> right;   //右孩子
+            BSTNode<T> left;    //左孩子
+            BSTNode<T> parent;  //父节点
+
+            public BSTNode(T key, BSTNode<T> right, BSTNode<T> left, BSTNode<T> parent) {
+                this.key = key;
+                this.right = right;
+                this.left = left;
+                this.parent = parent;
+            }
+        }
+
+
+
+}
