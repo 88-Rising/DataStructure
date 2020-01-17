0d964e1660b17940ed7f221e8412c25bcd25cf66
88-Rising <348514635@qq.com>
2020-01-17 23:09:25 +0800

    BSTree(二叉搜索树)增加销毁方法和采用遍历的打印方法

--- BSTree/BSTree.java
+++ BSTree/BSTree.java
@@ -318,6 +318,7 @@ public class BSTree <T extends Comparable<T>>{
 
         if (tree.left != null)
             destroy(tree.left);
+
         if (tree.right != null)
             destroy(tree.right);
 
