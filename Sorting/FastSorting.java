5c7c49c77a47f03b2dfe7dda3a823b8d8db2bf6d
88-Rising <348514635@qq.com>
2020-02-27 22:16:58 +0800

    关于Sorting的基本概念

---
+++ Sorting/FastSorting.java
@@ -0,0 +1,70 @@
+package Sorting;
+
+import java.util.Arrays;
+
+/*
+* 快速排序：是冒泡排序的改进版，也是最好的一种组内排序，还涉及到分治和递归
+* 冒泡排序：其中记录的比较和交换是在相邻的单元中进行，每次交换只能上移或者下移一个单元，因而总的比较和移动次数比较多
+*
+* 快排的基本思想：
+*   1.先从数列中取一个数作为基准数（一般取第一个数）
+*   2.分区过程，把这个数大的全部放在它的右边小于或等于它的数去不放在它的左边（分区）
+*   3.在对左右区间重复第一步，第二部直到各个区间只有一个数为止（递归）
+*
+* 核心在于分治和递归
+* */
+public class FastSorting {
+    private static int partition(int[] arr, int low, int high) {
+        //指定左指针i和右指针j
+
+        //指定第一个数为基准值
+
+        //使用循环实现分区操作
+
+        //使用基准值填坑，这就是基准值的最终位置
+
+        //返回基准值的位置索引
+
+        return 0;
+    }
+    
+    private  static void quickSort(int[] arr,int low,int high){
+        //分区操作 把一个数组分成两个数组 返回分区界索引
+        int index=partition(arr,low,high);
+        
+        //对右分区进行快排
+        quickSort(arr,low,index-1);
+        
+        
+        //对右分区进行快排
+        quickSort(arr,index+1,high);
+        
+    }
+
+
+
+    public static void quickSort(int[] arr) {
+        int low =0;
+        int high=arr.length-1;
+        quickSort(arr,low,high);
+        
+    }
+    
+    public static void main(String[] args){
+        //给出无序数组
+        int[] arr={72,6,57,88,60,4,83,73,48,85};
+
+        //输出无序数组
+        System.out.println(Arrays.toString(arr));
+
+        //快速排序
+        quickSort(arr);
+
+        //输出有序数组
+        System.out.println(Arrays.toString(arr));
+    }
+
+    
+
+
+}
